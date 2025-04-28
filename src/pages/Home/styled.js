627bc921c5d4f2dc4423044b4705da3ad637e92a
heloisatanquella <heloisacristovao02@gmail.com>
2025-04-28 10:05:21 -0300

    chore: pass styled on px to rem

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,47 +1,52 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
-    max-width: 700px;
-    background-color: #FFF;
-    border-radius: 4px;
-    box-shadow: 0 0 20px rgba(0,0,0, 0.2);
-    padding: 30px;
-    margin: 80px auto;
-
-    h1{
-        font-size: 20px;
-        display: flex;
-        align-items: center;
-        flex-direction: row;
-
-        svg{
-            margin-right: 10px;
-        }
-    }
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 3.5rem;
 `;
 
-export const Form = styled.form `
-    margin-top: 30px;
+export const BoxInput = styled.div`
+  width: 100%;
+  max-width: 43.75rem;
+  background-color: #fff;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 1.25rem rgba(0, 0, 0, 0.2);
+  padding: 1.875rem;
+
+  h1 {
+    font-size: 1.25rem;
     display: flex;
+    align-items: center;
     flex-direction: row;
 
-    input{
-        flex:1;
-        border: 1px solid #DDD;
-        padding: 10px 15px;
-        border-radius: 4px;
-        font-size: 17px;
+    svg {
+      margin-right: 0.625rem;
     }
+  }
 `;
 
-export const SubmitButton = styled.button `
-    background: #852a54;
-    border: 0;
-    border-radius: 4px;
-    margin-left: 10px;
-    padding: 0 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const Form = styled.form`
+  margin-top: 1.875rem;
+  display: flex;
+  flex-direction: row;
 
+  input {
+    flex: 1;
+    border: 0.063rem solid #ddd;
+    padding: 0.625rem 0.938rem;
+    border-radius: 0.25rem;
+    font-size: 1.063rem;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  background: #852a54;
+  border: 0;
+  border-radius: 0.25rem;
+  margin-left: 0.625rem;
+  padding: 0 0.938rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
