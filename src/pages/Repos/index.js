a984157f1b04391cda5109264372c60181fa396a
heloisatanquella <heloisacristovao02@gmail.com>
2025-05-06 10:23:21 -0300

    feat: displaying details

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -1,8 +1,57 @@
+import { useEffect, useState } from "react";
 import React from "react";
+import * as S from "./styled";
+import * as I from "react-icons";
+import API from "../../services/api";
 import { useParams } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function Repos() {
   const { repositorio } = useParams();
+  const [repos, setRepos] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <h1 style={{ color: "#FFF" }}>{repositorio}</h1>;
+  useEffect(() => {
+    async function load() {
+      try {
+        const [repositorioData, issuesData] = await Promise.all([
+          API.get(`/repos/${repositorio}`),
+          API.get(`/repos/${repositorio}/issues`, {
+            params: {
+              state: "open",
+              per_page: 5,
+            },
+          }),
+        ]);
+        setRepos(repositorioData.data);
+        setIssues(issuesData.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    }
+    load();
+  }, [repositorio]);
+
+  if (loading) {
+    return (
+      <S.Loading>
+        <h1>Carregando...</h1>
+      </S.Loading>
+    );
+  } else {
+    return (
+      <S.Container>
+          <S.BackButton to="/">
+            <FaArrowLeft color="#000" size={30} />
+          </S.BackButton>
+        <S.Owner>
+          <img src={repos.owner?.avatar_url} alt={repos.owner?.login} />
+          <h1>{repos.name}</h1>
+          <p>{repos.description}</p>
+        </S.Owner>
+      </S.Container>
+    );
+  }
 }
